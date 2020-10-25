ca887dec728705773db8ff5d02d2a3edc1484247
Tim Mineard <tim.mineard@yahoo.com>
2020-10-25 13:50:02 -0700

    Workshop Week 2

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
+import { Text } from 'react-native';
 
 export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
@@ -151,4 +152,27 @@ export const postFavorite = campsiteId => dispatch => {
 export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
+});
+
+export const postComment = (
+    campsiteId,
+    rating,
+    author,
+    text
+) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    newComment.date = new Date().toISOString();  
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+}
+
+export const addComment = newComment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: newComment
 });
